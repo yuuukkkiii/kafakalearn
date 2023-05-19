f99de9c1f5deec566844123e4e8619442902df95
Maxwell <zhaihao@maxwell-gp.com.cn>
2023-05-19 16:16:16 +0800

    项目初始化

---
+++ kafkainit/consumer/src/main/java/com/example/kafka/kafkainit/consumer/ConsumerApplication.java
@@ -0,0 +1,15 @@
+package com.example.kafka.kafkainit.consumer;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author :zh
+ * @date : 2023/5/19
+ * @description :
+ */
+@SpringBootApplication
+public class ConsumerApplication {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ kafkainit/producer/src/main/java/com/example/kafka/kafkainit/producer/ProducerApplication.java
@@ -0,0 +1,16 @@
+package com.example.kafka.kafkainit.producer;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author :zh
+ * @date : 2023/5/19
+ * @description :
+ */
+@SpringBootApplication
+public class ProducerApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
